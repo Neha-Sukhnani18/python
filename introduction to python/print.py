f6088d76374e31249b61f6af96aaaf9172573eab
nehasukhnani18 <nehasukhnani@lusakaoaktree.school>
2026-01-09 16:27:05 +0200

    print

---
+++ introduction to python/print.py
@@ -0,0 +1,8 @@
+#printing a greeting message
+print("hello,world")
+#printing my name
+print("my name is Neha")
+#printing a fun fact about myself
+print("i love shopping")
+#performing and printing a simple calculation
+print("the result of 3 + 5 is:", 3 + 5)
