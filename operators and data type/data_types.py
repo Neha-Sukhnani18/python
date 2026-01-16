9218aece3ae76bca7be28450715604baf2f68d55
nehasukhnani18 <nehasukhnani@lusakaoaktree.school>
2026-01-16 16:44:56 +0200

    operators and data types

---
+++ operators and data type/data_types.py
@@ -0,0 +1,11 @@
+name="neha"
+age=14
+is_student=True
+weight=55.6
+print("data type of name is", type(name))
+print("data type of age is", type(age))
+print("data type of is_student is",type(is_student))
+print("data type of weight is", type(weight))
+weight=int(weight)
+print(weight)
+print("data type of weight is", type(weight))
