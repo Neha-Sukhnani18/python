1cf2bef73498034d1e257a07dc0f5fa7a04bc114
nehasukhnani18 <nehasukhnani@lusakaoaktree.school>
2026-02-27 16:28:32 +0200

    main2

---
+++ oop-1/main2.py
@@ -0,0 +1,11 @@
+class student:
+    grade=10
+    name="penguin"
+    def introduction(self):
+        print("hi i am a student")
+    def details(self):
+        print("my name is",self.name)
+        print("i study in grade",self.grade)
+ob = student()
+ob.introduction()
+ob.details()
