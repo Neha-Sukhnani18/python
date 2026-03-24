e247ec1e62bc60c417ce1a8c4fc6576a28c38d58
nehasukhnani18 <nehasukhnani@lusakaoaktree.school>
2026-03-24 17:33:41 +0200

    main

---
+++ denominator calculator using tkinter/main1.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+root = Tk()
+root.title("denomination counter")
+root.configure(bg='light blue')
+root.geometry("650×400")
+upload= Image.open("app_img.jpg")
+upload = upload.resize((300,300))
+image= ImageTk.PhotoImage(upload)
+
+label= Label(root, image=image, bg="light blue")
+label.place(x=180, y=20)
+
+label1=Label(
+    root,
+    text="hey User!welcome to denomination counter application"
+    bg="light blue"
+)
+label1.place(relx=0.5,y=340, anchor=CENTER)
+def msg():
+    MsgBox = messagebox.showinfo(
+        "alert",
+        "do you want to calculate the denomination count?"
+    )
+    if MsgBox == "ok":
+        topwin()
+button1 = Button(
+    root,
+    text="lets get started"
+    command=msg,
+    bg="brown",
+    fg="white"
+    )
+button1.place(x=260, y=360)
+
+def topwin():
+    top = Toplevel()
+    top.title("denomination calculator")
+    top.configure(bg="light grey")
+    top.geometry("600×350+50+50")
+
+    label = Label(top, text="enter total amount",bg="light grey")
