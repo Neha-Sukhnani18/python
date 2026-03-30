f38bc3b7b8d858708cfd43cfcef32d4d797f772c
nehasukhnani18 <nehasukhnani@lusakaoaktree.school>
2026-03-30 13:36:07 +0200

    homework

--- introduction to time and space/main1.py
+++ introduction to time and space/main1.py
@@ -1,6 +1,7 @@
 def fun1(n):
     return n*(n+1)/2
 print(fun1(4))
+#since there is only one function taking place for this algorithm the number of iteration is one (4*5)/2
 
 def fun2(n):
     sum=0
@@ -11,7 +12,7 @@ def fun2(n):
     return sum
 
 print(fun2(4))
-
+#for the second algorithm there is a loop that is also being run so the number of iterations will be four[1+1+1+1]/(1+2+3+4)
 def fun3(n):
     sum=0
 
@@ -21,3 +22,4 @@ def fun3(n):
             sum+=1
     return sum
 print(fun3(4))
+#for the third algorithm there are 2 loops running(nested loop) therefore; the number of iterations will be ten(1+2+3+4) and the algorithm will look like: 1+(1+1)+(1+1+1)+(1+1+1+1)
