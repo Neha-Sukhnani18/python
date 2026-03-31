3d8c4b354f378b4ae9ee6d5d866cebd22c080602
nehasukhnani18 <nehasukhnani@lusakaoaktree.school>
2026-03-31 16:43:11 +0200

    main

---
+++ asymptotic notations/main1.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    print("the number entered by the user is",n)
+    iteration+=1
+    print("total iterations done by the code is",iteration,"\n")
+
+printnumber(10)
+printnumber(20)
+print("\n With 'n' the time taken by our code wont change")
