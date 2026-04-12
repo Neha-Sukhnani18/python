2300932876f7eb9763fc64ed2dac27561af26628
nehasukhnani18 <nehasukhnani@lusakaoaktree.school>
2026-04-12 13:13:06 +0200

    homework

---
+++ mathematic magic 1/hw.py
@@ -0,0 +1,11 @@
+
+binary_1 = "010"
+decimal_1 = int(binary_1, 2)
+print(f"Enter your Binary: {binary_1}")
+print(f"Decimal : {decimal_1}")
+
+
+binary_2 = "1101"
+decimal_2 = int(binary_2, 2)
+print(f"\nEnter your Binary: {binary_2}")
+print(f"Decimal : {decimal_2}")
