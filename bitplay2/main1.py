1ae20ec39902ade81000f8acf6a4546a064dec51
nehasukhnani18 <nehasukhnani@lusakaoaktree.school>
2026-04-19 13:47:17 +0200

    main

---
+++ bitplay2/main1.py
@@ -0,0 +1,14 @@
+#program to check if user input numbers are equal without using any comparision operator.
+def checkIfSame(number1, number2):
+
+# user XOR operator as a^a is always 0
+ if ((number1^number2)!=0):
+  print("numbers are not equal")
+ else:
+  print("both numbers are equal")
+
+#taking input
+number1 = int(input("enter the first number to compare:" ))
+number2= int(input("enter the second number to compare:"))
+
+checkIfSame(number1, number2)
