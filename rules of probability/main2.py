896da78eb9f2effaf3994382747fb3fb52967f4e
nehasukhnani18 <nehasukhnani@lusakaoaktree.school>
2026-09-11 16:31:14 +0200

    main2

---
+++ rules of probability/main2.py
@@ -0,0 +1,13 @@
+#Create the two sets of the two guest lists
+set1 = {'A','B','C','D','E'}
+set2 = {'B','D','V','X','Y','Z'}
+
+#Find union of two sets
+union = set1.intersection(set2)
+
+#Converting set into list
+#to find total guests to be invited in the party
+total_guests = list(union)
+
+print("Total guests to be invited in the party are:", len(total_guests))
+print("Guests List:", total_guests) 
